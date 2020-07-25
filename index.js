ce3ea83742b84f4dee8ab1951e85fc654bf4e1bc
derekb123 <derekbutvin@gmail.com>
2020-07-25 04:37:37 +0000

    recursive function working for single dimension array. Not working for multi-dimension array yet.

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+// function sumItems(array) {
+//   array.forEach((element) => {
+//     if (Array.isArray(element)) {
+//       sumItems(element);
+//     }
+//     else {
+//       if (array.length > 1) {
+//         array.shift();
+//         return array[0] + sumItems(array);
+//       }
+//       else {
+//         return array[0];
+//       }
+//     }
+//   });
+// };
 
-module.exports = sumItems;
+// const testArr = [4,2,[3,4]];
+
+// console.log(sumItems(testArr));
+
+function sumItems(array) {
+  if (array.length > 1) {
+    array.shift();
+    return array[0] + sumItems(array);
+  }
+  else {
+    return array[0];
+  }
+};
+
+const testArr = [4,2,3,4];
+
+console.log(sumItems(testArr));
+
+// function sumItems(array) {
+//   if (array.length = 1) {
+//     return array[0];
+//   }
+//   else {
+//     for (element in array) {
+      
+//     }
+//     if (Array.isArray(array)) {
+      
+//     }
+//   }
+// });
+// };
+
+// function sumItems(array) {
+//   let final = 0;
+//   for (item in array) {
+//     final += item;
+//   }
+//   return item;
+// };
+
+// module.exports = sumItems;foreach(array)
