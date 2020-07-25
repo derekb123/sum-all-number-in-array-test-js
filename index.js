57bd5de08a274f1916f0ca69fe3d8ef2ab05b9bf
derekb123 <derekbutvin@gmail.com>
2020-07-25 20:17:31 +0000

    finalized recursive function that sums array plus nested arrays

--- index.js
+++ index.js
@@ -1,37 +1,103 @@
+//**SMALLER ARRAY THAT WORKS**/
+
 // function sumItems(array) {
-//   array.forEach((element) => {
-//     if (Array.isArray(element)) {
-//       sumItems(element);
-//     }
-//     else {
-//       if (array.length > 1) {
-//         array.shift();
-//         return array[0] + sumItems(array);
-//       }
-//       else {
-//         return array[0];
-//       }
-//     }
-//   });
+//   if (array.length > 1) {
+//     return array.shift() + sumItems(array);
+//   }
+//   else {
+//     return array[0];
+//   }
 // };
 
-// const testArr = [4,2,[3,4]];
+// const testArr = [4,2,3,4];
 
 // console.log(sumItems(testArr));
 
+
 function sumItems(array) {
   if (array.length > 1) {
-    array.shift();
-    return array[0] + sumItems(array);
+    if (Array.isArray(array[0])) {
+      console.log(array[0]);
+      return sumItems(array.shift()) + sumItems(array);
+    }
+    else {
+      return array.shift() + sumItems(array);
+    }
   }
   else {
     return array[0];
   }
 };
 
-const testArr = [4,2,3,4];
+const testArr = [[4,2],3,4];
 
 console.log(sumItems(testArr));
+
+
+// function sumItems(array) {
+//   if (array.length > 1) {
+//     if (Array.isArray(array[0])) {
+//       sumItems(array[0]);
+//     }
+//     else{
+//       array.shift();
+//       console.log(array);
+//       return array[0] + sumItems(array);
+//     }
+//   }
+//   else {
+//     return array[0];
+//   }
+// };
+
+//   const testArr = [1,2,[3,4]];
+
+//   console.log(sumItems(testArr));
+
+
+// function sumItems(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (Array.isArray(array[i])) {
+//       return array[i] + sumItems(array[i-1]);
+//     }
+//     else {
+//       return array
+//     }
+//   }
+// };
+
+//   const testArr = [1,2,3,4];
+
+//   console.log(sumItems(testArr));
+
+
+
+
+
+// function sumItems(array) {
+//   for (let element of array) {
+//     if (Array.isArray(element)) {
+//       sumItems(element);
+//     }
+//     else {
+//       if (array.length > 1) {
+//         array.shift();
+//         if (Array.isArray(array[0])) {
+//           sumItems(array[0]);
+//         }
+//         else{
+//         return array[0] + sumItems(array);
+//         }
+//       else {
+//         return array[0];
+//       }
+//     }
+//   }
+// };
+
+// const testArr = [4,2,[3,4]];
+
+
 
 // function sumItems(array) {
 //   if (array.length = 1) {
